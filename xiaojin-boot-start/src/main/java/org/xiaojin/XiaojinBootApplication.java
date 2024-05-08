88db9e6ad8c49777ac92729ac97cac699d8b0bbd
chenyanwei <446725026@qq.com>
2024-05-08 16:41:09 +0800

    create base module

---
+++ xiaojin-boot-start/src/main/java/org/xiaojin/XiaojinBootApplication.java
@@ -0,0 +1,13 @@
+package org.xiaojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaojinBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaojinBootApplication.class, args);
+	}
+
+}
---
+++ xiaojin-boot-start/src/test/java/org/xiaojin/xiaojinboot/XiaojinBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xiaojin.xiaojinboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaojinBootStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
